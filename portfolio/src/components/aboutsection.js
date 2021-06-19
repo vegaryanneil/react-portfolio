1e0449a7e19e30843e8fa1373988420acf710785
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-06-19 01:09:40 -0400

    added to about section, added a pages folder with an aboutus.js

---
+++ portfolio/src/components/aboutsection.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const aboutSection = () => {
+    return (
+
+    )
+}
+
+export default aboutSection;
---
+++ portfolio/src/pages/AboutUs.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const aboutUs = () => {
+    return(
+
+    )
+}
+
+export default aboutUs;
