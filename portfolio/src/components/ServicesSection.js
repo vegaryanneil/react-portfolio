f35ffd8834ec71d65977c4c52bb1a6fa893ccba0
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-06-19 22:27:27 -0400

    Added another component called services

---
+++ portfolio/src/components/ServicesSection.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+// Import icons?
+
+const ServicesSection = () => {
+    return (
+        <div className="services">
+            <div className="description">
+                <h2>High <span>Quality</span> Services</h2>
+                <div className="cards">
+                    <div className="card">
+                        <div className="icon">
+                          <img src="{pseudoicon" alt="" className="src" />
+                          <h3>Dummy Text</h3>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                          <img src="{pseudoicon" alt="" className="src" />
+                          <h3>Dummy Text</h3>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                          <img src="{pseudoicon" alt="" className="src" />
+                          <h3>Dummy Text</h3>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                          <img src="{pseudoicon" alt="" className="src" />
+                          <h3>Dummy Text</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ServicesSection;
