abc8b4a4f98527129dd35e164d87ce91882ece1e
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-06-20 00:33:16 -0400

    added FAQ section component

--- portfolio/src/components/ServicesSection.js
+++ portfolio/src/components/ServicesSection.js
@@ -11,25 +11,25 @@ const ServicesSection = () => {
                     <div className="card">
                         <div className="icon">
                           <img src="{pseudoicon" alt="" className="src" />
-                          <h3>Dummy Text</h3>
+                          <h3>Dummy Text 1</h3>
                         </div>
                     </div>
                     <div className="card">
                         <div className="icon">
                           <img src="{pseudoicon" alt="" className="src" />
-                          <h3>Dummy Text</h3>
+                          <h3>Dummy Text 2</h3>
                         </div>
                     </div>
                     <div className="card">
                         <div className="icon">
                           <img src="{pseudoicon" alt="" className="src" />
-                          <h3>Dummy Text</h3>
+                          <h3>Dummy Text 3</h3>
                         </div>
                     </div>
                     <div className="card">
                         <div className="icon">
                           <img src="{pseudoicon" alt="" className="src" />
-                          <h3>Dummy Text</h3>
+                          <h3>Dummy Text 4</h3>
                         </div>
                     </div>
                 </div>
